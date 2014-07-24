f0f95b67efc0bcab32d41e91b732f6f4d62ce29c
Christian Chisholm <christian.l.chisholm@gmail.com>
2014-07-23 19:23:19 -0700

    added recent log to logfiles

---
+++ logfiles.rb
@@ -0,0 +1,18 @@
+commit 1213bc4574eeab3caac59f09e379237ad0eda268
+Author: Christian Chisholm <christian.l.chisholm@gmail.com>
+Date:   Wed Jul 23 19:22:20 2014 -0700
+
+    Added logfiles
+
+commit 068aaaa04caf7e720a3477280d6fe756473e0fc9
+Author: Christian Chisholm <christian.l.chisholm@gmail.com>
+Date:   Wed Jul 23 19:14:15 2014 -0700
+
+    Changed file2
+
+commit 03de6c5415ba6a8cd6fae8663b7a48a31386c2c9
+Author: Christian Chisholm <christian.l.chisholm@gmail.com>
+Date:   Wed Jul 23 19:13:58 2014 -0700
+
+    Change file1
+
